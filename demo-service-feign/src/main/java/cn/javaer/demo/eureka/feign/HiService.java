7f0ddfbf60675c02ce80ef3944b0c169de983609
zhangpeng <java@javaer.cn>
2017-07-28 17:05:50 +0800

    update

--- demo-service-feign/src/main/java/cn/javaer/demo/eureka/feign/HiService.java
+++ demo-service-feign/src/main/java/cn/javaer/demo/eureka/feign/HiService.java
@@ -3,7 +3,7 @@ package cn.javaer.demo.eureka.feign;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("DEMO-EUREKA-CLIENT-APPLICATION")
+@FeignClient(name = "DEMO-EUREKA-CLIENT-APPLICATION", fallback = HiServiceHystrix.class)
 // 可开启安全配置
 // @FeignClient(name = "DEMO-EUREKA-CLIENT-APPLICATION",configuration = HiConfiguration.class)
 public interface HiService
---
+++ demo-service-feign/src/main/java/cn/javaer/demo/eureka/feign/HiServiceHystrix.java
@@ -0,0 +1,16 @@
+package cn.javaer.demo.eureka.feign;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhangpeng
+ */
+@Component
+public class HiServiceHystrix implements HiService
+{
+    @Override
+    public String sayHiFromClientOne()
+    {
+        return "Hi, ERROR";
+    }
+}
