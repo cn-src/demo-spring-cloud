565bce117776bc721f82c6bd568d411907a3d6df
zhangpeng <java@javaer.cn>
2017-07-28 14:37:18 +0800

    init commit

---
+++ demo-eurekaserver/src/main/java/cn/javaer/demo/eurekaserver/DemoEurekaserverApplication.java
@@ -0,0 +1,12 @@
+package cn.javaer.demo.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEurekaserverApplication.class, args);
+	}
+}
