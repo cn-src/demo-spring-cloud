9daadebfd895cc455e13c3458e8e209a5d355794
zhangpeng <java@javaer.cn>
2017-07-28 16:54:32 +0800

    update

--- demo-service-ribbon/src/main/java/cn/javaer/demo/eureka/ribbon/DemoEurekaServiceRibbonApplication.java
+++ demo-service-ribbon/src/main/java/cn/javaer/demo/eureka/ribbon/DemoEurekaServiceRibbonApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class DemoEurekaServiceRibbonApplication
 {
     public static void main(String[] args)
--- demo-service-ribbon/src/main/java/cn/javaer/demo/eureka/ribbon/HelloService.java
+++ demo-service-ribbon/src/main/java/cn/javaer/demo/eureka/ribbon/HelloService.java
@@ -1,5 +1,6 @@
 package cn.javaer.demo.eureka.ribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,9 +17,14 @@ public class HelloService
         this.restTemplate = restTemplate;
     }
     
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService()
     {
         return restTemplate.getForObject("http://DEMO-EUREKA-CLIENT-APPLICATION/hi", String.class);
     }
     
+    public String hiError()
+    {
+        return "hi,sorry,error!";
+    }
 }
